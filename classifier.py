7c077636c8e3b3e15f9924f2acbe73012f9bbb79
Kacper <kacper.piotrowski1711@gmail.com>
2026-05-07 22:28:14 +0200

    Add train logic

--- classifier.py
+++ classifier.py
@@ -19,3 +19,20 @@ def open_file_and_save(file_name):
         sys.exit("File not found, exiting...")
     return file_data
 
+def train(data):
+    eg1_counts = {}
+    type_counts = {}
+    eg2_counts = {}
+    type1_counts = {}
+    for row in data:
+        if row[2] not in type_counts:
+            type_counts[row[2]] = {}
+        if row[2] not in eg2_counts:
+            eg2_counts[row[2]] = {}
+        if row[2] not in type1_counts:
+            type1_counts[row[2]] = {}
+        eg1_counts[row[2]] = eg1_counts.get(row[2], 0) + 1
+        type_counts[row[2]][row[0]] = type_counts[row[2]].get(row[0], 0) + 1
+        eg2_counts[row[2]][row[3]] = eg2_counts[row[2]].get(row[3], 0) + 1
+        type1_counts[row[2]][row[1]] = type1_counts[row[2]].get(row[1], 0) + 1
+    return eg1_counts, type_counts, eg2_counts, type1_counts
